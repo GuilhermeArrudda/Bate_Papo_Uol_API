2e990ca594eb1fc890f7855230ede71ae298e962
Guilherme <guilhermepa3@gmail.com>
2022-01-29 09:08:09 -0300

    adds get and post participants route

--- index.js
+++ index.js
@@ -1,15 +1,85 @@
 import express, { json } from 'express';
 import cors from 'cors';
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from 'dotenv';
+import joi from 'joi'
+import dayjs from 'dayjs'
+dotenv.config();
 
-const app = express();
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
-app.use(json());
-app.use(cors());
+const server = express();
+server.use(json());
+server.use(cors());
 
-app.get('/hello', (req, res) => {
-    res.send('teste')
+const participantsSchema = joi.object({
+  name: joi.string().required()
 });
 
-app.listen(5000, () => {
+
+server.get('/participants', async (req, res) => {
+
+    try {
+
+        await mongoClient.connect()
+        const dbBatePapo = mongoClient.db("batePapoUol");
+        const participantsCollection = dbBatePapo.collection('participants');
+
+        const participantsOn = await participantsCollection.find().toArray()
+        
+        res.send(participantsOn)
+    } catch {
+        res.sendStatus(500)
+    }
+    mongoClient.close()
+});
+
+server.post('/participants', async (req, res) => {
+    const validation = participantsSchema.validate(req.body)
+
+    if(validation.error){
+      const error = validation.error.details.map(detail => detail.message)
+      
+      res.status(422).send(error)
+      return;
+    }
+
+    try {
+        await mongoClient.connect()
+        const dbBatePapo = mongoClient.db("batePapoUol");
+        const participantsCollection = dbBatePapo.collection("participants")
+        const messagesCollection = dbBatePapo.collection("messages")
+    
+        const participantsOn = await participantsCollection.find({}).toArray()
+        let participantsList = participantsOn.map(e => e.name)
+        const newParticipant = req.body
+
+        if(participantsList.includes(newParticipant.name)){
+            res.status(409).send('Name unavailable.')
+            return;
+        }
+    
+          await participantsCollection.insertOne({newParticipant, lastStatus: Date.now()});
+    
+          let newStatusMsg = {
+            from: newParticipant.name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs(Date.now()).format('HH:mm:ss')
+          }
+          await messagesCollection.insertOne(newStatusMsg)
+    
+          res.sendStatus(201)
+    
+      } catch {
+        res.sendStatus(500)
+      }
+    
+      mongoClient.close()
+    
+    });
+
+server.listen(5000, () => {
     console.log('Running app in http://localhost:5000')
-});
+});  
