4607006edb4f18d04a277abb3735520e90befce4
Guilherme <guilhermepa3@gmail.com>
2022-01-31 09:53:20 -0300

    adds get and post messages route

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ const participantsSchema = joi.object({
   name: joi.string().required()
 });
 
+const messageSchema = joi.object({
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().required().valid('message', 'private_message')
+});
+
 
 server.get('/participants', async (req, res) => {
 
@@ -25,7 +31,7 @@ server.get('/participants', async (req, res) => {
         const dbBatePapo = mongoClient.db("batePapoUol");
         const participantsCollection = dbBatePapo.collection('participants');
 
-        const participantsOn = await participantsCollection.find().toArray()
+        const participantsOn = await participantsCollection.find({}).toArray()
         
         res.send(participantsOn)
     } catch {
@@ -35,6 +41,7 @@ server.get('/participants', async (req, res) => {
 });
 
 server.post('/participants', async (req, res) => {
+    const newParticipant = req.body
     const validation = participantsSchema.validate(req.body)
 
     if(validation.error){
@@ -52,7 +59,6 @@ server.post('/participants', async (req, res) => {
     
         const participantsOn = await participantsCollection.find({}).toArray()
         let participantsList = participantsOn.map(e => e.name)
-        const newParticipant = req.body
 
         if(participantsList.includes(newParticipant.name)){
             res.status(409).send('Name unavailable.')
@@ -79,6 +85,65 @@ server.post('/participants', async (req, res) => {
       mongoClient.close()
     
     });
+
+server.post('/messages', async (req,res) => {
+
+  const user = req.headers.user;
+  const newMessageUser = req.body;
+  await mongoClient.connect();
+  const dbBatePapo = mongoClient.db('batePapoUol')
+  const chatUser = await dbBatePapo.collection('participants').findOne({ name: user });
+  const messageUser = {...newMessageUser, from: user, time: dayjs().format('HH:mm:ss')};
+
+  const validation = messageSchema.validate(newMessageUser);
+    if (validation.error || chatUser || user === "") {
+        res.status(422).send(validation.error);
+        return;
+    }
+
+    try {
+        await dbBatePapo.collection("messages").insertOne(messageUser)
+        res.sendStatus(201);
+        mongoClient.close();
+    } catch {
+        res.sendStatus(500);
+        mongoClient.close();
+    }
+})
+
+server.get('/messages', async (req, res) => {
+
+  const messagesLimit = parseInt(req.query.limit);
+  const user = req.headers.user;
+  await mongoClient.connect();
+  const dbBatePapo = mongoClient.db('batePapoUol');
+  const messagesCollection = await dbBatePapo.collection('messages').find({}).toArray()
+  
+  if(!messagesLimit){
+    res.send(messagesCollection)
+    mongoClient.close();
+    return;
+  };
+  
+  try {
+    const messagesCollection = await dbBatePapo.collection('messages').find(
+      {$or: [
+        { type: 'status' }, 
+        { type: 'message' }, 
+        { to: user }, 
+        { from: user }
+      ]}
+      ).toArray();
+
+    res.send(messagesCollection.slice(-messagesLimit));
+    mongoClient.close();
+
+  } catch (error) {
+    res.sendStatus(500)
+    mongoClient.close();
+  }
+
+});
 
 server.listen(5000, () => {
     console.log('Running app in http://localhost:5000')
